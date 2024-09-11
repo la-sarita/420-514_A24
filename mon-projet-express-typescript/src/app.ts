5229987daa8ce20c0d5e105a2590e78b4f1405e2
la-sarita <sara.boumehraz@collegemv.qc.ca>
2024-09-11 13:26:15 -0400

    Ajout de la documentation de l'API

--- mon-projet-express-typescript/src/app.ts
+++ mon-projet-express-typescript/src/app.ts
@@ -2,11 +2,35 @@ import express, { Request, Response, NextFunction } from 'express';
 import userRoutes from './routes/user.route';
 import productRoutes from './routes/product.route';
 import { errorMiddleware } from './middlewares/error.middleware';
-
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 // Middleware de parsing du JSON
 app.use(express.json());
+
+// Définir les options de Swagger
+const swaggerOptions = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'User API',
+        version: '1.0.0',
+        description: 'A simple API to manage users',
+      },
+    },
+    apis: ['./src/routes/*.route.ts'], // Fichier où les routes de l'API sont définies
+  };
+  
+  // Générer la documentation à partir des options
+  const swaggerDocs = swaggerJsdoc(swaggerOptions);
+  
+  // Servir la documentation Swagger via '/api-docs'
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+  
+  // Autres routes et middleware Express
+  app.use(express.json());
+  
 
 // Route de base
 app.get('/', (req: Request, res: Response) => {
--- mon-projet-express-typescript/src/routes/user.route.ts
+++ mon-projet-express-typescript/src/routes/user.route.ts
@@ -4,6 +4,32 @@ import { UserController } from '../controllers/user.controller';
 const router = Router();
 const userController = new UserController();
 
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Retrieve a list of users
+ *     description: Retrieve a list of users from the API. Can be used to populate a list of users in your system.
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                  id:
+ *                     type: integer
+ *                     example: 1
+ *                  name:
+ *                     type: string
+ *                     example: John Doe
+ *                  mail:
+ *                     type: string
+ *                     example : john.doe@example.com
+ */
 router.get('/users', userController.getAllUsers);
 
 export default router;
